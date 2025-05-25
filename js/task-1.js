ecd235c5b58a49abe0b452f6c99b46b6f91fb4bc
Derya Almaz <deryaalmazz@icloud.com>
2025-05-26 00:12:56 +0300

    task 1 fixed

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,8 @@
+const categories = document.querySelectorAll('#categories li.item');
+console.log("Number of categories:", categories.length);
 
-const ul = document.querySelectorAll('ul li.item')
-console.log("Number of categories:", ul.length)
-
-ul.forEach(e => {
-    console.log("Category:", e.firstElementChild.textContent);
-    const li = e.querySelectorAll('ul li'); 
-    console.log("Elements:", li.length)
+categories.forEach(category => {
+    console.log("Category:", category.firstElementChild.textContent);
+    const elements = category.querySelectorAll('ul li');
+    console.log("Elements:", elements.length);
 });
